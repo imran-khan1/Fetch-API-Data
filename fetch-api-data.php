59913f70b15bdeca304d47002df72ee0487463fb
Imran Khan <ik.aics@gmail.com>
2023-02-02 11:33:36 +0500

    Update fetch-api-data.php

--- fetch-api-data.php
+++ fetch-api-data.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin name: Fetch API Data
  * Plugin URI: https://codeinform.pk
@@ -11,35 +12,16 @@
  */
 
 // If this file is access directly, abort!!!
-defined( 'ABSPATH' ) or die( 'Unauthorized Access' );
+defined('ABSPATH') or die('Unauthorized Access');
 
-function codeinform_get_send_data() {
-
-    $url = 'https://jsonplaceholder.typicode.com/users';
-    
-    $arguments = array(
-        'method' => 'GET'
-    );
-
-	$response = wp_remote_get( $url, $arguments );
-
-	if ( is_wp_error( $response ) ) {
-		$error_message = $response->get_error_message();
-		return "Something went wrong: $error_message";
-	} else {
-
-    echo '<pre>';
-		var_dump( wp_remote_retrieve_body( $response ) );
-		echo '</pre>';
-	}
-}	
 
 /**
- * Register a custom menu page to view the information queried.
+ * Register a custom admin menu page to view API response data.
  */
-function codeinform_register_fetch_api_menu_page() {
+function codeinform_register_fetch_api_menu_page()
+{
 	add_menu_page(
-		__( 'Fetch API Data Settings', 'fetch-apis' ),
+		__('Fetch API Data Settings', 'fetch-apis'),
 		'Fetch API',
 		'manage_options',
 		'api-fetch.php',
@@ -49,4 +31,30 @@ function codeinform_register_fetch_api_menu_page() {
 	);
 }
 
-add_action( 'admin_menu', 'codeinform_register_fetch_api_menu_page' );
+add_action('admin_menu', 'codeinform_register_fetch_api_menu_page');
+
+
+/**
+ * codeinform_get_send_data() function to Query API
+ */
+function codeinform_get_send_data()
+{
+
+	$url = 'https://jsonplaceholder.typicode.com/users';
+
+	$arguments = array(
+		'method' => 'GET'
+	);
+
+	$response = wp_remote_get($url, $arguments);
+
+	if (is_wp_error($response)) {
+		$error_message = $response->get_error_message();
+		return "Something went wrong: $error_message";
+	} else {
+
+		echo '<pre>';
+		var_dump(wp_remote_retrieve_body($response));
+		echo '</pre>';
+	}
+}
